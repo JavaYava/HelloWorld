5f20273c296aead2bad9cb2992d1bcabf079c34d
324062843 <324062843@C-9S608J1.tdsb.on.ca>
2016-09-14 13:25:34 -0400

    AboutMe

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,19 @@
-
+		/*className.java
+		 * About Me
+		 * Sept/12/16
+		 * Emira Refai
+		 */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		
+
+		System.out.format("%-10s%3s","Emira","refaiGray/n");
+		System.out.format("%-10s%3s","MsKemp","Runnymede Collegiate/n");
+	    System.out.format("%-10s%1s","\"Go\"","\"Ravens\"");
 	}
+	
 
 }
