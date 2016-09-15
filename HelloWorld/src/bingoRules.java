c6cfe1c133edd0dc51a10134220502d31e617ffb
324062843 <324062843@C-9S608J1.tdsb.on.ca>
2016-09-15 12:59:57 -0400

    Bingo Rules

---
+++ HelloWorld/src/bingoRules.java
@@ -0,0 +1,14 @@
+
+public class bingoRules {
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		System.out.format("Bingo Card/n");
+		System.out.format("1.The caller randomly pulls a numbered bingo ball./n");
+		System.out.format("2.The number is placed on the bingo board and called out./n");
+		System.out.format("3.Players look for the number on their bingo board and called out./n");
+		System.out.format("4.If the number is located, it is marked off./n");
+        System.out.format("5. Steps 1 to 4 are repeated until a player matches the BINGO pattern./n");
+        System.out.format("6. The winning player yells BINGO./n");
+        
+}
+}
