e33c5a4658d59a34d38972cbf68b1d3f2e2c7035
324062843 <324062843@C-9S608J1.tdsb.on.ca>
2016-09-20 13:27:39 -0400

    fix \n

--- HelloWorld/src/TicTacToeRules.java
+++ HelloWorld/src/TicTacToeRules.java
@@ -10,13 +10,13 @@ public class TicTacToeRules {
 	// TODO Auto-generated method stub
 			System.out.format("Tic Tac Rules\n");
 			  System.out.format("\n");
-			System.out.format("Tic Tac Toe is a game for 2 players.");
+			System.out.format("Tic Tac Toe is a game for 2 players.\n");
 			  System.out.format("\n");
 			System.out.format("1.A grid of 9 squares is drawn\n");
 			  System.out.format("\n");
 			System.out.format("2.The first player draws an X in an empty sqaure.\n");
 			  System.out.format("\n");
-			System.out.format("3.The second player draws an O in an empty square./n");
+			System.out.format("3.The second player draws an O in an empty square.\n");
 			  System.out.format("\n");
 			System.out.format("4.Steps 2 and 3 are repeated until a Tic Tac Pattern is created:\n");
 			  System.out.format("\n");
