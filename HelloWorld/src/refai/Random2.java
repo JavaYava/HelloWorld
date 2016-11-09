f2ad36bcd36713c823247cc071e5340d7e980b5c
324062843 <324062843@C-9S608J1.tdsb.on.ca>
2016-11-09 13:29:41 -0500

    Fixed it

--- HelloWorld/src/refai/Random2.java
+++ HelloWorld/src/refai/Random2.java
@@ -16,9 +16,9 @@ public static void main(String[] args) {
 	int dice1;  
 	int dice2;
 	
-	dice1=(int)(Math.random()*6)+1;
+	dice1=(int)(Math.random()*11)+1;
 	
-	dice2=(int)(Math.random()*6)+1;
+	dice2=(int)(Math.random()*11)+1;
 	
 	if (dice1>dice2)
 		
