71bbc20f802c17576a382b88b378aca8e05701ea
324062843 <324062843@C-9S608J1.tdsb.on.ca>
2016-11-11 13:22:48 -0500

    Exercise 9

---
+++ HelloWorld/src/refai/GuessingGame.java
@@ -0,0 +1,77 @@
+package refai;
+import java.util.Scanner;
+
+
+/*GuessingGame.java
+		 * This program is a number guessing game played between the user and the computer.
+		 * Nov/11/16
+		 * Emira Refai
+		 */
+
+
+	
+public final class GuessingGame {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner scan=new Scanner(System.in);
+		
+		int a; int b;
+		
+		System.out.println("Enter a number between 1 and 20:");
+	
+	   	a=scan.nextInt();
+	   	
+	   	b= (int) (Math.random()*20)+1;
+	   	
+	   	System.out.println("Computer's Number:" + b);
+	   	
+	   	System.out.println("Player's Number:" + a);
+	   	
+	   	if (a<b)
+	   		
+	   		System.out.println("Better luck next time.");
+	   	
+	   	
+	   	
+	   	
+	  else if (a==b)
+	   		
+	   		System.out.println("You won!");
+	 
+	  else if (a>b)
+		  
+		  System.out.println("Better luck next time.");
+	   	
+	   	
+	   	
+	   	
+	   	
+	   	
+	   		
+	   				
+	   	
+	   	
+	   			
+	     	
+	     	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+	
+
+}
