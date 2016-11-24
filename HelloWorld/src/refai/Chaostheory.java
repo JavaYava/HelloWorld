45e406e7af77db9c321904a5f35a3dcfd12b340d
324062843 <324062843@C-9S608J1.tdsb.on.ca>
2016-11-24 16:42:24 -0500

    Chaos theory exercise 15

---
+++ HelloWorld/src/refai/Chaostheory.java
@@ -0,0 +1,37 @@
+package refai;
+
+import java.util.Scanner;
+
+
+/*Chaostheory.java
+ * This program is a chaos theory that produces a new x 50 times.
+ * Nov/24/16
+ * Emira Refai
+ */
+
+public class Chaostheory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan=new Scanner(System.in);
+
+
+		for (int x=1; x<=50; x++)
+		{
+
+			x=2*x*1 - x;
+
+			System.out.println(x);
+		}
+
+
+	}
+
+}
+
+
+
+
+
+
