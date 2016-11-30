21d7a6a812d8c8bac1e0c2fb97291d039ac6e8ab
324062843 <324062843@C-9S608J1.tdsb.on.ca>
2016-11-30 13:36:09 -0500

    Done

--- HelloWorld/src/refai/RemoveString.java
+++ HelloWorld/src/refai/RemoveString.java
@@ -14,47 +14,41 @@ public class RemoveString {
 		Scanner scan=new Scanner(System.in);
 
 		int index ;
-		
+
 		int index2; 
-		
-		int index3;
-		
-		int index4;
-		
+
+
+
 		String sentence;
 
 		String string2;
 
 		System.out.println("Enter a sentence:");
-		
+
 		sentence = scan.nextLine();
-		
+
 		System.out.println("Enter a string");
-		
+
 		string2 = scan.nextLine();
-		
+
 		index =(sentence.indexOf(string2));
-		index2=(sentence.indexOf(sentence));
-		
-		
-		
-		index3=(sentence.length()); 
-		index4 =(string2.length());
-		
-		
-		System.out.println(sentence.substring (0,index) +sentence.substring(index3,index4));
- 
+
+		index2 =(string2.length());
 
 
-		
-		
-		
-		
-		
- 
+		System.out.println(sentence.substring (0,index) +sentence.substring(index2+index));
+
+
+
+
+
+
+
+
+
 
 
 	}
 
-	
-	}
+
+}
