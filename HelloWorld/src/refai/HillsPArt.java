d907f8c000d54e542dc59329f8b866fcd2801779
324062843 <324062843@C-9S608J1.tdsb.on.ca>
2016-12-15 13:57:02 -0500

    Story + Hills Part1

---
+++ HelloWorld/src/refai/HillsPArt.java
@@ -0,0 +1,36 @@
+package refai;
+import java.util.Scanner;
+
+
+/*HillsPart
+ * This program is a updated verison of the number guessing game played between the user and the computer but in this verison you cna guess as many times until you win.
+ * Nov/22/16
+ * Emira Refai
+ */
+
+
+
+public final class HillsPArt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan=new Scanner(System.in);
+
+
+		
+		System.out.println("You awaken under a pile of rubble in a cold sweat, your vision blurry, head pounding as the memories flow back to you of what happened last night"
+				+"as you look around at the ashed remains of your village. With the smoke of the still burning houses and the sounds of screaming ringing in your ears as the scene won't stop replaying."
+				+ " You choke back a sob as it was just last night you lost everything in a wave of fire. You lay your eyes on a sword plunged into the ground, you step forward and grab the hilt as you pull the sword out stare into the "
+				+ "glistening sliver steel blade as you come to your decision venegence.");
+		
+		
+	
+		
+
+		
+		
+		
+		
+	}
+}
