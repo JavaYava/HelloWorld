04efdae5399368ce3c06ea6e79ea748c7efa6168
JavaYava <emira-refai@hotmail.com>
2016-12-23 08:11:26 -0500

    Add pics

--- Hills.java
+++ Hills.java
@@ -1,4 +1,5 @@
-package refai;
+
+
 import java.awt.Color;
 
 import hsa_new.Console;      
@@ -34,26 +35,27 @@ public class Hills
 		do{
 			c.println("You awaken under a pile of rubble in a cold sweat, your vision blurry, head \npounding as the memories flow back to you of what happened last night"
 					+"as you look around at the ashed remains of your village. With the smoke of the still burning houses and the sounds of screaming ringing in your ears as the scene won't stop replaying."
-					+ " You choke back a sob as it was just last night you lost everything in a wave of fire. You lay your eyes on a sword plunged into the ground, you step forward and grab the hilt as you pull the sword out stare into the "
+					+ " You choke back a sob as it was just last night you lost everything in a wave of fire. You lay your eyes on a sword plunged into the ground, you \nstep forward and grab the hilt as you pull the sword out stare into the "
 					+ "glistening sliver steel blade as you come to your decision venegence. "
-					+ "You dust off your clothes and look off in the direction of the "
-					+ "burnt path to the forest and start your adventure");
+					+ "You dust off your \nclothes and look off in the direction of the "
+					+ "burnt path to the forest and start \nyour adventure.");
 
-			c.println("Do you take the Forest or the Hills?");
+			c.println("\nDo you take the Forest or the Hills?");
 			direction=c.readLine();
 
 			if(direction.equalsIgnoreCase("Hills")){
 				c.clear();
-				c.println("The trail through the hills takes you up a steep path the deeper you venture into the hills the soft grass has long since turned into hard gravel as it crunches beneath your feet. It was a rather peaceful walk but"
+				c.println("The trail through the hills takes you up a steeper path the deeper you venture into the hills, the soft grass of forest has long since turned into hard gravel as it crunches beneath your feet. It was a rather peaceful walk but"
 						+ "you couldnt shake the feeling of being watched. The path leads you to a wooden bridge that streatches up into the mountains, you attempt to take a step on the bridge a man appears before as you cant believe your eyes. You take a step"
 						+ "back as the man strides over to on a stump by the bridge he dusts off his jacket and clears his throat as he begans to speak");
-				c.println("Man: Stop you must pay a toll to cross this bridge, but judging from the looks of you, you're poor so let's play a game, if you anwser my riddle correctly I'll let you pass. But if you anwser incorrectly it'll cost you your life ");
-				c.println("1.Yes");
-				c.println("2.No");
+				c.println("Man: Stop you must pay a toll to cross this bridge, but judging from the looks \nof you, you're poor so let's play a game, if you anwser my riddle correctly I'll let you pass. But if you anwser incorrectly it'll cost you your life ");
+				c.println("\n1.Yes");
+				c.println("\n2.No");
 				answer=c.readLine();
 				if(answer.equalsIgnoreCase("No")){
 					c.clear();
-					c.println("It wasn't a yes or no question. The man pulls a dagger out of his pocket and stabs you right in the gut and leaves you on the ground to bleed out. ");
+					c.println("It wasn't a yes or no question. The man pulls a dagger out of his pocket and \nstabs you right in the gut and leaves you on the ground to bleed out. ");
+					c.println("You died");
 				}else if (answer.equalsIgnoreCase("Yes")){
 					c.clear();
 					counter = 0;
@@ -66,7 +68,7 @@ public class Hills
 
 						if(answer.equalsIgnoreCase("Pain") || answer.equalsIgnoreCase("Wisdom")){
 							c.clear();
-							c.println("Wrong");
+							c.println("Wrong!");
 							counter++;	
 						}
 					}while(counter<2 && !answer.equalsIgnoreCase("Pride"));
@@ -76,7 +78,7 @@ public class Hills
 
 						c.clear();
 
-						c.println("Correct. The man sighs and lets you pass you come across the mountains and see a cave you enter a cave inside you see a"
+						c.println("Correct. The man sighs and lets you cross the bridge. You come across th mountains and see a cave you enter a cave inside you see a"
 								+ " sleeping dragon."
 								+ "do you try to sneak attack or rush in and attack it ");
 						c.println("1.Sneak Attack");
@@ -98,7 +100,7 @@ public class Hills
 										+ "stone echos in the cave. The dragon awakens you peer into the eyes of the beast,  and stand frozen in fear. You accept your fate as the dragon snaps its jaws open and shallows you whole ");
 
 							}else if (dice1>dice2){
-								c.println("You sucessfully sneak up on the dragon, you heave the sword and kill it WIN.  ");
+								c.println("You sucessfully sneak up on the dragon, you heave the sword and kill it.");
 
 							}else if(dice1==dice2){
 								c.println("You Win");
@@ -116,7 +118,10 @@ public class Hills
 
 							if(direction.equalsIgnoreCase("Dodge")){
 								c.clear();
-								//	roll dice here
+								
+								dice1=(int)(Math.random()*11)+2;
+								dice2=(int)(Math.random()*11)+2;
+								
 								if (dice1<dice2){
 									c.println("You seemed to have tripped on nothing, the dragons claw connects with your body and rips you apart");
 									c.println("You have died");
@@ -131,9 +136,10 @@ public class Hills
 									if(direction.equalsIgnoreCase("Dodge")){
 										c.println("The dragon expected you would try to roll out of the way again and breathes fire instead"
 												+ "burning you to a crisp.");
-										c.println("You have died.");
+										c.println("You died.");
 									}else if(direction.equalsIgnoreCase("Attack")){
-										//random dice rolls
+										dice1=(int)(Math.random()*11)+2;
+										dice2=(int)(Math.random()*11)+2;
 										if (dice1>dice2){
 
 											c.clear();
@@ -155,25 +161,22 @@ public class Hills
 														+ "find "+ anArray[0] +" "+ "gold pieces and" + anArray[1] + " "+"silver pieces");
 											}
 											if(direction.equalsIgnoreCase("No")){
-												c.println("Are you sure? You don't want to see whats in the chest?");
-												c.println("1.Yes");
-												c.println("2.No");
-												if(direction.equalsIgnoreCase("Yes")){
-													c.println("You cut out the dragons heart as a prize for your bravely and walk out of the dragons lair victorious. ");
+								
+													c.println("You choose to not look in the chest and instead you cut out the dragons heart as a more greater prize for your bravely and walk out of the dragons lair victorious. ");
 													c.println("You Win");
-												}
+												
 											}
 
 										}else if (dice1<dice2){
 											c.println("You missed the dragon and fell on your own sword. The steel cuts through you like paper"
 													+ "and you killed yourself.");
-											c.println("You have died");	
+											c.println("You died.");	
 										}
 									}
 								}
 							}else if(direction.equalsIgnoreCase("Attack")){
 								c.println("The dragon swipes at you mid swing and sends you flying into the air killing you instantly");
-								c.println("You have died");
+								c.println("You died.");
 							}
 
 						}
@@ -189,6 +192,35 @@ public class Hills
 
 				direction = c.readLine();
 
+				if(direction.equalsIgnoreCase("Left")){
+					c.clear();
+
+					c.println("The path leads you to a rushing a river, the trail continues on the other side ");
+					c.println("Do you attempt to cross the river on foot or try and find another way across?");
+					c.println("1.Cross");
+					c.println("2.Go Around");
+					direction = c.readLine();
+
+					if(direction.equalsIgnoreCase("cross")){
+
+						c.clear();
+						c.println("The river was alot deeper then you thought it would be, you become sumbmerged "
+								+ "under the water. You straggle as you try to swim back to the top but the current /nof the river is too strong as it bangs you against rocks.");
+
+						c.println("You died");
+
+					}
+					if(direction.equalsIgnoreCase("go around")){
+						c.clear();
+						c.println("You go around the river and try to look for a way to cross. Leading you further and further away from your original path as you get lost in the forest, never to be heard from again.");
+						c.println("You died.");
+					}
+
+				}
+
+
+
+				}
 				if(direction.equalsIgnoreCase("Right")){
 					c.clear();
 					c.println("The path on the right leads you to a cave.");
@@ -198,12 +230,17 @@ public class Hills
 
 					direction = c.readLine();
 
+					if(direction.equalsIgnoreCase("Stay Out")){
+						c.println("You decide to not go into the cave and continue walking. You get tired and \ndecide to take seat by a tree to rest, you close your eyes and drift off too sleep. \nYou awaken an hour later face to face"
+								+ "with a bear, the last moments of your life \nwere spent screaming in shock, as the bear bites into you throat.");
+						c.println("You died");
+					}
 					if (direction .equalsIgnoreCase("In")){
 						c.clear();
 
 						c.println("You muster your courage and walk into the cave, you try to avoid making any noise by side shuffling around to not"
-								+ " step on any of the animal bones that are littered around the floor you get into the rythm of stepping around the bones, as you reach towards the end of the cave you see it"
-								+ " a sleeping bear blocking you from the exit.");
+								+ " step on any of the animal bones that are littered around the floor,  as you reach towards the end of the cave you see it"
+								+ " a \nsleeping bear blocking you from the exit.");
 						c.println("What do you do?");
 						c.println("1.Attack");
 						c.println("2.Sneak Around It");
@@ -217,7 +254,7 @@ public class Hills
 
 							c.println("You charge in at the bear and pierce it right in the shoulder it roars in pain, you try to pull your sword out planning to finish it in one final blow. The sword doesn't come loose the beasr notices"
 									+ "your struggle and snaps its powerful jaws around your neck and kills you.");
-							c.println("You have died");
+							c.println("You died.");
 
 						}
 						if (direction.equalsIgnoreCase("Sneak around it")){
@@ -229,8 +266,8 @@ public class Hills
 
 							if (dice1<dice2){
 								c.clear();
-								c.println("You trip over your own two feet and fall onto the bear, it jolts awake and tears you to shreds."); //fix death scene
-								c.println("You have died");
+								c.println("You trip over your own two feet and fall onto the bear, it jolts awake and tears you to shreds."); 
+								c.println("You died.");
 							}
 
 
@@ -253,14 +290,32 @@ public class Hills
 
 									c.clear();
 									c.println("The river was alot deeper then you thought it would, you become sumbmerged "
-											+ "under the water. You straggle as you try to swim back to the top but the current of"
-											+ "the river is too strong as it bangs you against rocks. ");
-									c.println("You have died");
+											+ "under the water. ");
+									counter = 0;
+									do{
+										c.clear();
+										c.println("Do you try to escape?");
+										c.println("1.To Escape");
+										
+										answer=c.readLine();
+
+										if(answer.equalsIgnoreCase("escape")){
+											c.clear();
+											c.println("You try to stand up but fall back down.");
+											counter++;	
+										}
+									}while(counter<2);
+
+
+										
+									c.println("The current of the river is too strong and you've lost the strenght to swim back to the top.");
+									c.println("You died");
 
 								}
 								if(direction.equalsIgnoreCase("go around")){
 									c.clear();
-									c.println("You go around the river and get lost. and die in the forest");
+									c.println("You go around the river and try to look for a way to cross. Leading you further and further away from your original path as you get lost in the forest, never to be heard from again.");
+									c.println("You died.");
 								}
 
 							}
@@ -288,22 +343,16 @@ public class Hills
 
 
 				}
-			}
-
-			c.println("\n");
-			c.println("You have died");
+				c.println("\n");
+		
 			c.println("Type 1 to play again");
 			restart=c.readInt();
 			c.clear();
-		}while(restart==1);
+			}while(restart==1);
+
+			
+		}
 
 	}
-}
-
-
-
-
-
-
 
 
