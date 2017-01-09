71dcd0bba689d324e1d452db55efd532c636881b
324062843 <324062843@C-9S608J1.tdsb.on.ca>
2017-01-09 13:43:14 -0500

    FINISH DIS

--- HelloWorld/src/refai/ListofNames.java
+++ HelloWorld/src/refai/ListofNames.java
@@ -1,4 +1,5 @@
 package refai;
+
 import java.util.Scanner;
 
 /*RemoveString.java
@@ -10,21 +11,51 @@ public class ListofNames {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+
 		//Write a program that reads in a list of names from the keyboard, 
 		//then output the list twice, first in the order that it was input, and then in reverse order.
-	//The user should get to pick how many names will be in the list.
-		
-		
+		//The user should get to pick how many names will be in the list.
+
+
+
+		//Ask how many they want
+		//make array
+		//loop
+
 		Scanner scan = new Scanner(System.in);
-	
-		int n; 
+
+		String[] name;
+
+
+
+
+
+		System.out.println("How many names would you like to type in?");
+
+		name=new String[scan.nextInt()];
+
+		
+
+for(int a= 0; a<name.length; a++){
+
+			System.out.println("Type in your names");
+			
+			name[a]=scan.nextLine();
+
+		
+		}
+		
+		System.out.print(name);
+
+
 		
 		
-	System.out.println("Enter in the names one by one");
-	n=scan.nextInt();
+		
+		
+
+		
 	
-           double[] names = new do
+          
 	
 
 	
