a5c164b9e9d8bc633e324b48b6bdc75031773ec9
324062843 <324062843@C-9S608J1.tdsb.on.ca>
2017-01-12 13:24:30 -0500

    Fixed the spelling

---
+++ HelloWorld/src/refai/Mississippi.java
@@ -0,0 +1,91 @@
+package refai;
+
+//Write a program to spell out MISSISSIPPI using block letters like the following:
+
+//HINT: Use one method for each letter.
+
+
+//Mississippi
+
+public class Mississippi {
+
+	public static void main(String[] args){
+		printM();
+		printSpace();
+		printI();
+		printSpace();
+		printS();
+		printSpace();
+		printS();
+		printSpace();
+		printI();
+		printSpace();
+		printS();
+		printSpace();
+		printS();
+		printSpace();
+		printI();
+		printSpace();
+		printP();
+		printSpace();
+		printP();
+		printSpace();
+		printI();
+	}
+/**
+ * Draws a M 
+ */
+	public static void  printM(){
+		System.out.println("M     M");
+		System.out.println("MM   MM" );
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+	}
+	/**
+	 * Draws a I
+	 */
+	public static void printI(){
+		System.out.println("IIIII");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("IIIII");
+	}
+	/**
+	 * Draws a S
+	 */
+	public static void printS(){
+		System.out.println(" SSSSS");
+		System.out.println("S     S");
+		System.out.println("S");
+		System.out.println(" SSSSS");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS");
+	}
+	/**
+	 * Draws a P
+	 */
+	public static void printP(){
+		System.out.println("PPPPP ");
+		System.out.println("P    P");
+		System.out.println("P    P");
+		System.out.println("PPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+		
+	}
+	/**
+	 * Draws a space
+	 */
+	public static void printSpace(){
+		System.out.println();
+	}
+
+}
+
