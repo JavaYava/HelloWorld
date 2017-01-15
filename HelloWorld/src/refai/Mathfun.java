dcf51b2be75444454bd68b32c63b3780a0f4a6c9
JavaYava <emira-refai@hotmail.com>
2017-01-15 02:19:34 -0500

    Mathfun.java
    Add a way for it to restart the program.

--- HelloWorld/src/refai/Mathfun.java
+++ HelloWorld/src/refai/Mathfun.java
@@ -1,5 +1,3 @@
-package refai;
-
 import java.util.Scanner;
 /*MathFun.java
  * This program is a 4 system calculator.
@@ -23,19 +21,24 @@ public class Mathfun {
 
 			printAddition(num1, num2); 
 			//call method 1(Addition)
+			printRestart();
 		}
 		else if(anwser==2){
 
 			printSubtraction(num1,num2);
 			//call method 2(Subtraction)
+			printRestart();
 		}
 		else if(anwser==3){
 			printDivison(num1,num2);
 			//call method 3(Division)
+			printRestart();
 		}
 		else if (anwser==4){
 			printMultiply(num1,num2);
 			//call method 4(Multiply)
+			printRestart();
+
 		}
 
 	}
@@ -69,6 +72,49 @@ public class Mathfun {
 	//(method 4)
 	public static void printMultiply(int num1, int num2){
 		System.out.println(num1*num2);
+	}
+	/**
+	 * Restarts the program
+	 */
+	public static void printRestart(){
+		Scanner scan=new Scanner(System.in);
+
+		System.out.println("Type '1' to restart");
+		int restart=scan.nextInt();
+		do{
+
+			System.out.println("Type in two numbers you would like to use");
+			int anwser;
+			int num1=scan.nextInt();
+			int num2=scan.nextInt();
+			System.out.println("What would you like to do?");
+			System.out.println("Type 1, Addition");
+			System.out.println("Type 2, Subtraction");
+			System.out.println("Type 3, Divison");
+			System.out.println("Type 4, Multiply");
+			anwser=scan.nextInt();
+			if(anwser==1){
+
+				printAddition(num1, num2); 
+				//call method 1(Addition)
+			}
+			else if(anwser==2){
+
+				printSubtraction(num1,num2);
+				//call method 2(Subtraction)
+			}
+			else if(anwser==3){
+				printDivison(num1,num2);
+				//call method 3(Division)
+			}
+			else if (anwser==4){
+				printMultiply(num1,num2);
+				//call method 4(Multiply)
+
+			}
+
+		}while (restart==1);
+
 	}
 
 }
